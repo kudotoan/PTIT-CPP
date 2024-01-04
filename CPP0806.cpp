5eb2c947dcda09b967cad68379b6b19fb4d32eb2
Nguyễn Toàn <155508756+kudotoan@users.noreply.github.com>
2024-01-04 18:11:36 +0700

    file C++

---
+++ CPP0806.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+using namespace std;
+static int MaKH=1;
+static int MaMH=1;
+static int MaHD=1;
+class KhachHang;
+class MatHang;
+class HoaDon;
+map <string, KhachHang> KH;
+map <string, MatHang> MH;
+class KhachHang{
+	public:
+    	string MKH, ten, GT, NS, diachi;
+	    friend istream &operator>>(istream &in, KhachHang &a) {
+            // scanf("\n");
+	        a.MKH="KH";
+	        string tmp=to_string(MaKH++);
+	        while (a.MKH.length()+tmp.length()<5) a.MKH+='0';
+			a.MKH+=tmp;
+			getline(in>>ws,a.ten);
+			getline(in>>ws,a.GT);
+			getline(in>>ws,a.NS);
+			getline(in>>ws,a.diachi);
+            KH.insert({a.MKH,a});
+			return in;
+	    }
+        friend class HoaDon;
+};
+class MatHang {
+	public:
+        string MMH, tenMH, DVT;
+		int giamua, giaban;
+		friend istream &operator>>(istream& in, MatHang &a) {
+            // scanf("\n");
+            a.MMH="MH";
+            string tmp=to_string(MaMH++);
+            while(a.MMH.length()+tmp.length()<5) a.MMH+='0';
+            a.MMH+=tmp;
+            getline(in>>ws,a.tenMH);
+            getline(in>>ws,a.DVT);
+            in >> a.giamua;
+            in >> a.giaban;
+            in.ignore();
+            MH.insert({a.MMH,a});
+            return in;
+        }
+        friend class HoaDon;
+};
+class HoaDon {
+    private:
+        string MHD,MKH,MMH;
+        int sl;
+    public:
+        friend istream &operator>>(istream &in, HoaDon &a) {
+            // scanf("\n");
+            a.MHD="HD";
+            string tmp=to_string(MaHD++);
+            while(a.MHD.length()+tmp.length()<5) a.MHD+='0';
+            a.MHD+=tmp;
+            in >> a.MKH >> a.MMH >> a.sl;
+            return in;
+        }
+        friend ostream &operator<<(ostream &out, HoaDon a) {
+            out << a.MHD << " " << KH[a.MKH].ten << " " << KH[a.MKH].diachi << " " << MH[a.MMH].tenMH << " " << MH[a.MMH].DVT << " " << MH[a.MMH].giamua << " " << MH[a.MMH].giaban << " " << a.sl << " " << MH[a.MMH].giaban*a.sl << endl;
+            return out;
+        }
+};
+int main(){
+    KhachHang dskh[25];
+    MatHang dsmh[45];
+    HoaDon dshd[105];
+    ifstream KHinp("KH.in");
+    int N,M,K,i;
+    KHinp >> N;
+    KHinp.ignore();
+    for(i=0;i<N;i++) KHinp >> dskh[i];
+    KHinp.close();
+    ifstream MHinp("MH.in");
+    MHinp >> M;
+    MHinp.ignore();
+    for(i=0;i<M;i++) MHinp >> dsmh[i];
+    MHinp.close();
+    ifstream HDinp("HD.in");
+    HDinp >> K;
+    HDinp.ignore();
+    for(i=0;i<K;i++) HDinp >> dshd[i];
+    HDinp.close();
+    for(i=0;i<K;i++) cout << dshd[i];
+    return 0;
+}
